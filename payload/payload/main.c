34e7321da5410f167e2b9bb23c94a7b2e1f7e582
skar-hassmen <94719450+skar-hassmen@users.noreply.github.com>
2023-04-28 00:28:33 +0300

    [feature] implemented payload

---
+++ payload/payload/main.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <windows.h>
+
+
+int main() {
+    const wchar_t startDir[] = L"C:\\Windows\\*";
+    WIN32_FIND_DATA FindFileData;
+    HANDLE hf = FindFirstFileW(startDir, &FindFileData);
+
+    if (hf == INVALID_HANDLE_VALUE) return;
+
+    FILE* file = fopen("infoDir.txt", "w");
+
+    int i = 0;
+    do {
+        char temp[256] = { '\0' };
+        if (i > 1) {
+            sprintf(temp, "%ws", FindFileData.cFileName);
+            fputs(temp, file);
+            fputc('\n', file);
+        }
+        i++;
+    } while (FindNextFile(hf, &FindFileData));
+    
+    FindClose(hf);
+    fclose(file);
+	return 0;
+}
